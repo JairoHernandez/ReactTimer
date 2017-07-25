60d3bd061e1a2b11e997969d10de906fdd580980
JairoHernandez <jairomh@hotmail.com>
2017-07-25 12:07:37 -0500

    Wire up controls component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
     getInitialState: function() {
@@ -18,6 +19,12 @@ var Countdown = React.createClass({
             switch(this.state.countdownStatus) {
                 case 'started':
                     this.startTimer();
+                    break;
+                case 'stopped':
+                    this.setState({count: 0});
+                case 'paused':
+                    clearInterval(this.timer); // leaves count wherever it was
+                    this.timer = undefined;
                     break;
             }
         }
@@ -39,14 +46,26 @@ var Countdown = React.createClass({
         });
     },
 
+    handleStatusChange: function(newStatus) {
+        this.setState({countdownStatus: newStatus});
+    },
+
     render: function() {
 
-        var {count} = this.state;
+        var {count, countdownStatus} = this.state;
+        var renderControlArea = () => {
+            console.log(countdownStatus);
+            if (countdownStatus !== 'stopped') {
+                return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+            } else {
+                console.log('test');
+                return <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+            }
+        };
         return (
             <div>       
                 <Clock totalSeconds={count}/>
-                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
-                <p>Countdown.jsx</p>
+                {renderControlArea()}
             </div>
         );
     }
