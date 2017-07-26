b249d24f458ba1552e891f1bbe4064ee2c5ea195
JairoHernandez <jairomh@hotmail.com>
2017-07-26 17:13:01 -0500

    Finished Project Solution Timer p1

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,8 +1,30 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
+
+
 var Timer = React.createClass({
+
+    getInitialState: function() {
+        return {
+            count: 0,
+            timerStatus: 'stopped'
+        }
+    },
+
+    handleStatusChange: function(newTimerStatus) {
+        console.log('newTimerStatus:', newTimerStatus);
+    },
+
     render: function() {
+        var {count, timerStatus} = this.state;
+
         return (
-            <p>Timer.jsx</p>
+            <div>
+                <h1 className="page-title">Timer App</h1>
+                <Clock totalSeconds={count}/>
+                <Controls countdownStatus={timerStatus} onStatusChange={this.handleStatusChange}/>
+            </div>
         );
     }
 });
